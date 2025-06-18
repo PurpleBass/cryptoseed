ab71d75c638a3575946e95dba6f16323dd4431f1
PurpleBass <121925228+PurpleBass@users.noreply.github.com>
2025-06-17 22:18:48 -0600

    chore: purge unused and obsolete files (i18n, KDF worker, nonceGenerator, REMOVE-ME-UNUSED-FILES.txt)

--- src/utils/useKdf.ts
+++
@@ -1,9 +0,0 @@
-
-const worker = new Worker(new URL('../workers/pbkdf2Worker.ts', import.meta.url), { type: 'module' });
-
-export function deriveKey(payload: { password: string; salt: Uint8Array; iterations: number }): Promise<CryptoKey> {
-  return new Promise<CryptoKey>((res) => {
-    worker.onmessage = (e) => res(e.data as CryptoKey);
-    worker.postMessage(payload);
-  });
-}
